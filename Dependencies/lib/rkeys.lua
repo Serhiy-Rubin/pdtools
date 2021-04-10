73bed21885cdc1f9c05cdf6ce82b35ebec460541
Serhiy-Rubin <39163828+Serhiy-Rubin@users.noreply.github.com>
2021-04-11 02:25:30 +0300

    Update rkeys.lua

--- Dependencies/lib/rkeys.lua
+++ Dependencies/lib/rkeys.lua
@@ -1,372 +1,221 @@
 --[[
+   Register HotKey for MoonLoader
    Author: DonHomka
-   E-mail: idonhomka@gmail.com
+   Functions:
+      - int id = registerHotKey(table keys, bool pressed, function callback)
+      - unRegisterHotKey:
+         > bool result, int count = unRegisterHotKey(table keys)
+         > bool result = unRegisterHotKey(int id)
+      - isHotKeyDefined:
+         > bool result, int id = isHotKeyDefined(table keys)
+         > bool result, table keys = isHotKeyDefined(int id)
+      - table keys, bool end = getCurrentHotKey(bool show_name_keys)
+      - table keys = getAllHotKey()
+   HotKey data:
+      - table keys                  Return table keys for active hotkey
+      - bool pressed                True - wasKeyPressed() / False - isKeyDown()
+      - function callback           Call this function on active hotkey
+   E-mail: a.skinfy@gmail.com
    VK: http://vk.com/DonHomka
-   TeleGramm: http://t.me/iDonHomka
+   TeleGramm: http://t.me/DonHomka
    Discord: DonHomka#2534
+
+   Last update:
+      #1.1.0:
+      - Fixed bug isHotKeyHotKey
+      - Deleted block-functions
 ]]
 local vkeys = require 'vkeys'
-local wm = require 'windows.message'
-local bitex = require 'bitex'
-local ffi = require 'ffi'
 
-local tCurKeys = {}
-local tModKeys = { [vkeys.VK_MENU] = true, [vkeys.VK_SHIFT] = true, [vkeys.VK_CONTROL] = true }
-local tModKeysList = { vkeys.VK_MENU, vkeys.VK_LMENU, vkeys.VK_RMENU, vkeys.VK_SHIFT, vkeys.VK_RSHIFT, vkeys.VK_LSHIFT, vkeys.VK_CONTROL, vkeys.VK_LCONTROL, vkeys.VK_RCONTROL }
-local tMessageTrigger = {
-                        [wm.WM_KEYDOWN] = true,
-                        [wm.WM_SYSKEYDOWN] = true,
-                        [wm.WM_KEYUP] = true,
-                        [wm.WM_SYSKEYUP] = true,
-                        [wm.WM_LBUTTONDOWN] = true,
-                        [wm.WM_LBUTTONDBLCLK] = true,
-                        [wm.WM_LBUTTONUP] = true,
-                        [wm.WM_RBUTTONDOWN] = true,
-                        [wm.WM_RBUTTONDBLCLK] = true,
-                        [wm.WM_RBUTTONUP] = true,
-                        [wm.WM_MBUTTONDOWN] = true,
-                        [wm.WM_MBUTTONDBLCLK] = true,
-                        [wm.WM_MBUTTONUP] = true,
-                        [wm.WM_XBUTTONDOWN] = true,
-                        [wm.WM_XBUTTONDBLCLK] = true,
-                        [wm.WM_XBUTTONUP] = true,
-                        [wm.WM_MOUSEWHEEL] = true
-                     }
-local tRewriteMouseKeys = {
-                           [wm.WM_LBUTTONDOWN] = vkeys.VK_LBUTTON,
-                           [wm.WM_LBUTTONUP] = vkeys.VK_LBUTTON,
-                           [wm.WM_RBUTTONDOWN] = vkeys.VK_RBUTTON,
-                           [wm.WM_RBUTTONUP] = vkeys.VK_RBUTTON,
-                           [wm.WM_MBUTTONDOWN] = vkeys.VK_MBUTTON,
-                           [wm.WM_MBUTTONUP] = vkeys.VK_MBUTTON,
-                        }
-local tXButtonMessage = {
-                           [wm.WM_XBUTTONUP] = true,
-                           [wm.WM_XBUTTONDOWN] = true,
-                           [wm.WM_XBUTTONDBLCLK] = true,
-                        }
-local tXButtonMouseData = {
-                           vkeys.VK_XBUTTON1,
-                           vkeys.VK_XBUTTON2
-                        }
-local tDownMessages = {
-                        [wm.WM_KEYDOWN] = true,
-                        [wm.WM_SYSKEYDOWN] = true,
-                        [wm.WM_LBUTTONDOWN] = true,
-                        [wm.WM_RBUTTONDOWN] = true,
-                        [wm.WM_MBUTTONDOWN] = true,
-                        [wm.WM_XBUTTONDOWN] = true,
-                        [wm.WM_LBUTTONDBLCLK] = true,
-                        [wm.WM_RBUTTONDBLCLK] = true,
-                        [wm.WM_MBUTTONDBLCLK] = true,
-                        [wm.WM_XBUTTONDBLCLK] = true,
-                        [wm.WM_MOUSEWHEEL] = true
-                     }
-local tHotKeys = {}
-local tActKeys = {}
-local hkId = 0
-local mod = {}
+vkeys.key_names[vkeys.VK_LMENU] = "LAlt"
+vkeys.key_names[vkeys.VK_RMENU] = "RAlt"
+vkeys.key_names[vkeys.VK_LSHIFT] = "LShift"
+vkeys.key_names[vkeys.VK_RSHIFT] = "RShift"
+vkeys.key_names[vkeys.VK_LCONTROL] = "LCtrl"
+vkeys.key_names[vkeys.VK_RCONTROL] = "RCtrl"
 
-mod._VERSION = "2.1.1"
+local tHotKey = {}
+local tKeyList = {}
+local tKeysCheck = {}
+local iCountCheck = 0
+local tBlockKeys = {[vkeys.VK_LMENU] = true, [vkeys.VK_RMENU] = true, [vkeys.VK_RSHIFT] = true, [vkeys.VK_LSHIFT] = true, [vkeys.VK_LCONTROL] = true, [vkeys.VK_RCONTROL] = true}
+local tModKeys = {[vkeys.VK_MENU] = true, [vkeys.VK_SHIFT] = true, [vkeys.VK_CONTROL] = true}
+local tBlockNext = {}
+local module = {}
+module._VERSION = "1.1.0"
+module._MODKEYS = tModKeys
+module._LOCKKEYS = false
 
-mod._MODKEYS = tModKeysList
-
-mod._LOCKKEYS = false
-
-mod.vkeys = {
-   VK_WHEELDOWN = 0x100,
-   VK_WHEELUP = 0x101
-}
-
-mod.vkeys.names = {
-   [mod.vkeys.VK_WHEELDOWN] = "Mouse Wheel Down",
-   [mod.vkeys.VK_WHEELUP] = "Mouse Wheel Up",
-}
-
-local id_to_name = function(id)
-   local name = vkeys.id_to_name(id) or mod.vkeys.names[id]
-   return name
-end
-local HIWORD = function(param)
-   return bit.rshift(bit.band(param, 0xffff0000), 16);
-end
-local splitsigned = function(n) -- ÑÏÀÑÈÁÎ WINAPI.lua è GITHUB è Chat mimgui
-   n = tonumber(n)
-   local x, y = bit.band(n, 0xffff), bit.rshift(n, 16)
-   if x >= 0x8000 then x = x-0xffff end
-   if y >= 0x8000 then y = y-0xffff end
-   return x, y
-end
-
-addEventHandler("onWindowMessage", function (message, wparam, lparam)
-   if tMessageTrigger[message] then
-      local scancode = bitex.bextract(lparam, 16, 8)
-      local keystate = bitex.bextract(lparam, 30, 1)
-      local keyex = bitex.bextract(lparam, 24, 1)
-      if tXButtonMessage[message] then
-         local btn = HIWORD(wparam)
-         wparam = tXButtonMouseData[btn]
-      elseif message == wm.WM_MOUSEWHEEL then
-         local btn, delta = splitsigned(ffi.cast('int32_t', wparam))
-         if delta < 0 then
-            wparam = mod.vkeys.VK_WHEELDOWN
-         elseif delta > 0 then
-            wparam = mod.vkeys.VK_WHEELUP
-         end
-      elseif tRewriteMouseKeys[message] then
-         wparam = tRewriteMouseKeys[message]
-      end
-      local keydown = mod.isKeyExist(wparam)
-      if tDownMessages[message] then
-         if not keydown and keystate == 0 then
-            table.insert(tCurKeys, wparam)
-            if tModKeys[wparam] then
-               local exKey = mod.getExtendedKeys(wparam, scancode, keyex)
-               if exKey then
-                  table.insert(tCurKeys, exKey)
-               end
-            end
-         end
-         for k, v in ipairs(tHotKeys) do
-            if v.aType ~= 3 and (tActKeys[v.id] == nil or v.aType == 2)
-               and ((v.aType == 1 and keystate == 0) or v.aType == 2)
-               and mod.isKeyComboExist(v.keys)
-               and (not mod.isKeysExist(tModKeysList, v.keys) and not mod.isKeysExist(tModKeysList) or mod.isKeysExist(tModKeysList, v.keys))
-               and (mod.onHotKey == nil or (mod.onHotKey and mod.onHotKey(v.id, v) ~= false))
-            then
-               lua_thread.create(function()
-                  wait(0)
-                  v.callback(v)
-               end)
-               tActKeys[v.id] = true
-            end
-            if v.isBlock and (tActKeys[v.id] or v.aType == 2) or (tActKeys[v.id] and not v.isBlock and mod._LOCKKEYS) then
-               consumeWindowMessage()
-            end
-         end
-      else
-         for k, v in ipairs(tHotKeys) do
-            if v.aType == 3 and keystate == 1 and mod.isKeyComboExist(v.keys)
-               and (mod.onHotKey == nil or (mod.onHotKey and mod.onHotKey(v.id, v) ~= false))
-               then
-               v.callback(v)
-            end
-         end
-         if keydown then
-            local pos = mod.getKeyPosition(wparam)
-            table.remove(tCurKeys, pos)
-            if tModKeys[wparam] then
-               local exKey = mod.getExtendedKeys(wparam, scancode, keyex)
-               if exKey then
-                  pos = mod.getKeyPosition(exKey)
-                  table.remove(tCurKeys, pos)
-               end
-            end
-         end
-      end
-      if message == wm.WM_MOUSEWHEEL then
-         local pos = mod.getKeyPosition(wparam)
-         table.remove(tCurKeys, pos)
-      end
-      for k, v in ipairs(tHotKeys) do
-         if v.aType == 2 or tActKeys[v.id] and not mod.isKeyComboExist(v.keys) then
-            tActKeys[v.id] = nil
-         end
-      end
-   elseif message == wm.WM_KILLFOCUS then
-      tCurKeys = {}
-   end
-end)
-
-function mod.registerHotKey(keycombo, activationType, isBlock_or_callback, callback)
-   local newId = hkId + 1
-   tHotKeys[#tHotKeys + 1] = {
-      keys = keycombo,
-      aType = activationType,
-      callback = type(isBlock_or_callback) == "function" and isBlock_or_callback or callback,
-      id = newId,
-      isBlock = type(isBlock_or_callback) == "boolean" and isBlock_or_callback or false
-   }
-   hkId = hkId + 1
-   return newId
-end
-
-function mod.isHotKeyDefined(keycombo_or_id)
-   local bool = false
-   local count = 0
-   if type(keycombo_or_id) == "number" then
-      for k, v in ipairs(tHotKeys) do
-         if v.id == keycombo_or_id then
-            bool = true
-            count = count + 1
-         end
-      end
-   elseif type(keycombo_or_id) == "table" then
-      for k, v in ipairs(tHotKeys) do
-         if mod.compareKeys(v.keys, keycombo_or_id) then
-            bool = true
-            count = count + 1
-         end
+local function getKeyNum(id)
+   for k, v in pairs(tKeyList) do
+      if v == id then
+         return k
       end
    end
-   return bool, count
+   return 0
 end
 
-function mod.getHotKey(keycombo_or_id)
-   local bool = false
-   local data = {}
-   if type(keycombo_or_id) == "number" then
-      for k, v in ipairs(tHotKeys) do
-         if v.id == keycombo_or_id then
-            bool = true
-            data = v
-            break
-         end
-      end
-   elseif type(keycombo_or_id) == "table" then
-      for k, v in ipairs(tHotKeys) do
-         if mod.compareKeys(v.keys, keycombo_or_id) then
-            bool = true
-            data = v
-            break
-         end
-      end
+function module.isHotKeyHotKey(keys, keys2)
+   if type(keys) ~= "table" then
+      print("[RKeys | isHotKeyHotKey]: Bad argument #1. Value \"", tostring(keys), "\" is not table.")
+      return false
+   elseif type(keys2) ~= "table" then
+      print("[RKeys | isHotKeyHotKey]: Bad argument #2. Value \"", tostring(keys2), "\" is not table.")
+      return false
+   else
+      return table.concat(keys, " ") == table.concat(keys2, " ")
    end
-   return bool, data
 end
 
-function mod.changeHotKey(id, keycombo)
+function module.isKeyModified(id)
+   if type(id) ~= "number" then
+      print("[RKeys | isKeyModified]: Bad argument #1. Value \"", tostring(id), "\" is not number.")
+      return false
+   end
+   return (tModKeys[id] or false) or (tBlockKeys[id] or false)
+end
+
+function module.isModifiedDown()
    local bool = false
-   for k, v in ipairs(tHotKeys) do
-      if v.id == id then
+   for k, v in pairs(tModKeys) do
+      if isKeyDown(k) then
          bool = true
-         v.keys = keycombo
+         break
       end
    end
    return bool
 end
 
-function mod.unRegisterHotKey(keycombo_or_id)
-   local bool = false
-   local count = 0
-   local idsToRemove = {}
-   if type(keycombo_or_id) == "number" then
-      for k, v in ipairs(tHotKeys) do
-         if v.id == keycombo_or_id then
-            bool = true
-            count = count + 1
-            table.insert(idsToRemove, k)
-         end
-      end
-   elseif type(keycombo_or_id) == "table" then
-      for k, v in ipairs(tHotKeys) do
-         if mod.compareKeys(v.keys, keycombo_or_id) then
-            bool = true
-            count = count + 1
-            table.insert(idsToRemove, k)
+lua_thread.create(function ()
+   while true do
+      wait(0)
+      local tDownKeys = module.getCurrentHotKey()
+      for k, v in pairs(tHotKey) do
+         if #v.keys > 0 then
+            local bool = true
+            for i = 1, #v.keys do
+               if i ~= #v.keys and (getKeyNum(v.keys[i]) > getKeyNum(v.keys[i + 1]) or getKeyNum(v.keys[i]) == 0) then
+                  bool = false
+                  break
+               elseif i == #v.keys and (v.pressed and not wasKeyPressed(v.keys[i]) or not v.pressed and not isKeyDown(v.keys[i])) or (#v.keys == 1 and module.isModifiedDown()) then
+                  bool = false
+                  break
+               end
+            end
+            if bool and ((module.onHotKey and module.onHotKey(k, v.keys) ~= false) or module.onHotKey == nil) then
+               v.callback(k, v.keys)
+            end
          end
       end
    end
-   for k, v in ipairs(idsToRemove) do
-      table.remove(tHotKeys, v)
-   end
-   return bool, count
+end)
+
+function module.registerHotKey(keys, pressed, callback)
+   tHotKey[#tHotKey + 1] = {keys = keys, pressed = pressed, callback = callback}
+   return #tHotKey
 end
 
-function mod.isKeyComboExist(keycombo, keylist)
-   keylist = keylist or tCurKeys
-   local b = false
-   local i = 1
-   for k, v in ipairs(keylist) do
-      if v == keycombo[i] then
-         if i == #keycombo then
-            b = true
+function module.getAllHotKey()
+   return tHotKey
+end
+
+function module.changeHotKey(id, newkeys)
+   if type(id) ~= "number" then
+      print("[RKeys | changeHotKey]: Bad argument #1. Value \"", tostring(keys), "\" is not number.")
+      return false
+   elseif type(newkeys) ~= "table" then
+      print("[RKeys | changeHotKey]: Bad argument #2. Value \"", tostring(newkeys), "\" is not table.")
+      return false
+   else
+      local bool = false
+      if module.isHotKeyDefined(id) then
+         tHotKey[id].keys = newkeys
+         bool = true
+      end
+      return bool
+   end
+end
+
+function module.unRegisterHotKey(keys_or_id)
+   local result = false
+   local count = 0
+   if type(keys_or_id) == "number" and tHotKey[keys_or_id] then
+      tHotKey[keys_or_id] = nil
+      result = true
+      count = nil
+   elseif type(keys_or_id) == "table" then
+      while module.isHotKeyDefined(keys_or_id) do
+         local _, id = module.isHotKeyDefined(keys_or_id)
+         tHotKey[id] = nil
+         result = true
+         count = count + 1
+      end
+      local id = 1
+      local tNewHotKey = {}
+      for k, v in pairs(tHotKey) do
+         tNewHotKey[id] = v
+         id = id + 1
+      end
+      tHotKey = tNewHotKey
+   else
+      print("[RKeys | unRegisterHotKey]: Bad argument #1. Value \"", tostring(keys_or_id), "\" is not number or table.")
+      return false, -1
+   end
+   return result, count
+end
+
+function module.isHotKeyDefined(keys_or_id)
+   if type(keys_or_id) == "number" and tHotKey[keys_or_id] then
+      return true, tHotKey[keys_or_id].keys
+   elseif type(keys_or_id) == "table" then
+      local bool, hkId = false, -1
+      for k, v in pairs(tHotKey) do
+         if module.isHotKeyHotKey(keys_or_id, v.keys) then
+            bool = true
+            hkId = k
             break
          end
-         i = i + 1
       end
+      return bool, hkId
+   else
+      print("[RKeys | isHotKeyDefined]: Bad argument #1. Value \"", tostring(keys_or_id), "\" is not number or table.")
+      return false, -1
    end
-   return b
+   return false, -1
 end
 
-function mod.compareKeys(keycombo, keycombotwo)
-   local b = true
-   for k, v in ipairs(keycombo) do
-      if keycombotwo[k] ~= v then
-         b = false
-         break
+function module.getKeysName(keys)
+   if type(keys) ~= "table" then
+      print("[RKeys | getKeysName]: Bad argument #1. Value \"", tostring(keys), "\" is not table.")
+      return false
+   else
+      local tKeysName = {}
+      for k, v in ipairs(keys) do
+         tKeysName[k] = vkeys.id_to_name(v)
       end
+      return tKeysName
    end
-   return b
 end
 
-function mod.isKeyExist(keyid, keylist)
-   keylist = keylist or tCurKeys
-   for k, v in ipairs(keylist) do
-      if tonumber(v) == tonumber(keyid) then
-         return true
-      end
-   end
-   return false
-end
-
-function mod.isKeysExist(keyids, keylist)
-   keylist = keylist or tCurKeys
-   for k, v in ipairs(keyids) do
-      for kk, vv in ipairs(keylist) do
-         if v == vv then
-            return true
+function module.getCurrentHotKey(show_name_keys)
+   local show_name_keys = show_name_keys or false
+   local tCurKeys = {}
+   for k, v in pairs(vkeys) do
+      if tBlockKeys[v] == nil then
+         local num, down = getKeyNum(v), isKeyDown(v)
+         if down and num == 0 then
+            tKeyList[#tKeyList + 1] = v
+         elseif num > 0 and not down then
+            tKeyList[num] = nil
          end
       end
    end
-   return false
-end
-
-function mod.getKeyPosition(keyid, keylist)
-   keylist = keylist or tCurKeys
-   for k, v in ipairs(keylist) do
-      if v == keyid then
-         return k
-      end
+   local i = 1
+   for k, v in pairs(tKeyList) do
+      tCurKeys[i] = show_name_keys == false and v or vkeys.id_to_name(v)
+      i = i + 1
    end
-   return nil
+   return tCurKeys, #tKeyList > 0 and not module.isKeyModified(tKeyList[#tKeyList]) or false
 end
 
-function mod.getExtendedKeys(keyid, scancode, extend)
-   local newKeyId = nil
-   if keyid == vkeys.VK_MENU then
-      if extend == 1 then
-         newKeyId = vkeys.VK_RMENU
-      else
-         newKeyId = vkeys.VK_LMENU
-      end
-   elseif keyid == vkeys.VK_SHIFT then
-      if scancode == 42 then
-         newKeyId = vkeys.VK_LSHIFT
-      elseif scancode == 54 then
-         newKeyId = vkeys.VK_RSHIFT
-      end
-   elseif keyid == vkeys.VK_CONTROL then
-      if extend == 1 then
-         newKeyId = vkeys.VK_RCONTROL
-      else
-         newKeyId = vkeys.VK_LCONTROL
-      end
-   end
-   return newKeyId
-end
-
-function mod.getKeys(keyname, keyscan, keyex)
-   keyname = keyname or false
-   local szKeys = {}
-   for k, v in ipairs(tCurKeys) do
-      table.insert(szKeys, ("%s%s"):format(tostring(v), (keyname and ":" .. id_to_name(v) or "")))
-   end
-   return szKeys
-end
-
-function mod.getCountKeys()
-   return #tCurKeys
-end
-
-return mod
+return module
